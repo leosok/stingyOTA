ee9ffe5abe84b899c7483e6f78a9aa55e12bd033
fischteichit@gmx.de <charley>
2023-02-27 23:52:17 +0100

    added setup.py to later upload to pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="micropython-stingy-ota",
+    version="0.1",
+    author="Leonid Sokolov",
+    author_email="l.sokolov@mailbox.org",
+    description="An OTA updater for MicroPython devices.",
+    url="https://github.com/leosok/stingyOTA",
+    packages=["stingy-ota"]
+)
